782ee4520fa3c97e5e9b2db4b1e1872f52c16533
Bengabo <ben@bengabo.com>
2020-12-06 18:10:42 +0100

    transition with opacity between splash and home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,52 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  myCustomPageTransition = ((baseEl: any, opts?: any) => {
+    console.log("opts.enteringEl" + opts.enteringEl);
+    console.log("opts.leavingEl" + opts.leavingEl);
+
+    var anim1 = this.animationCtrl.create()
+      .addElement(opts.leavingEl)
+      .duration(2500)
+      .iterations(1)
+      .easing('ease-out')
+      .fromTo('opacity', '1', '0.0')
+
+    var anim2 = this.animationCtrl.create()
+      .addElement(opts.enteringEl)
+      .duration(2500)
+      .iterations(1)
+      .easing('ease-out')
+      .fromTo('opacity', '0.0', '1')
+
+      var transition = this.animationCtrl.create()
+      .addElement(opts.enteringEl)
+      .duration(2500)
+      .iterations(1)
+      .addAnimation([anim1, anim2])
+
+    return transition
+  });
+
+  // myCustomPageTransition = ((baseEl: any, opts?: any) => {
+  //   console.log("opts.enteringEl" + opts.enteringEl);
+  //   console.log("opts.leavingEl" + opts.leavingEl);
+  //   var anim = this.animationCtrl.create()
+  //     .addElement(opts.leavingEl)
+  //     .duration(1000)
+  //     .iterations(1)
+  //     .easing('ease-out')
+  //     .fromTo('opacity', '1', '0.0')
+  //   return anim
+  // });
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    public router: Router
+    public router: Router,
+    private animationCtrl: AnimationController
   ) {
     this.initializeApp();
   }
